806d5456a4d06045f676732845cc6ff6fff6c157
Haris Malik <harismalik0326@gmail.com>
2022-10-04 12:53:05 +0100

    Exercise1.3 Complete

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+from math import sqrt
+
+def isPrime(number):
+    for i in range(2,int(sqrt(number)+1)):
+        if number%i == 0:
+            return False
+    return True
 
 def primes(number_of_primes):
     list = []
+    n = 2
+    while len(list) < number_of_primes:
+        if isPrime(n):
+            list.append(n)
+        n += 1
     return list
+
+print(primes(5))
